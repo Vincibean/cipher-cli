28ccf581a6fc7c502027dd6bfe3a24c8ddd7c7f7
Vincibean <andrebessi00@gmail.com>
2020-08-09 19:34:43 +0100

    first draft of CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,36 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
 import Lib
+import System.Console.CmdArgs
 
-main :: IO ()
-main = someFunc
+data Op = Encrypt | Decrypt deriving (Show, Data, Typeable)
+
+data Algo = Caesar {op :: Op, msg :: String, numKey :: Int} | Vigenere {op :: Op, msg :: String, stringKey :: String} deriving (Show, Data, Typeable)
+
+caesar =
+  Caesar
+    { numKey = def, -- &= help "Number of threads to use" &= name "j" &= typ "NUM"&= argPos 0
+      op =
+        enum
+          [ Encrypt &= help "Encrypt message",
+            Decrypt &= help "Decrypt message"
+          ],
+      msg = def &= help "Message" -- &= name "m"
+    }
+    &= help "Use Caesar's algorithm"
+
+vigenere =
+  Vigenere
+    { stringKey = def,
+      op =
+        enum
+          [ Encrypt &= help "Encrypt message" &= name "e",
+            Decrypt &= help "Decrypt message" &= name "d"
+          ],
+      msg = def &= help "Message" &= name "m"
+    }
+    &= help "Use Vigenere's algorithm"
+
+main = print =<< cmdArgs (modes [caesar, vigenere])

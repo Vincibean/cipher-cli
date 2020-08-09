1ed5f22cb346ab8559d1aebfb44e90a6d27a7c46
Vincibean <andrebessi00@gmail.com>
2020-08-09 16:51:44 +0100

    record syntax to the rescue

--- src/Types.hs
+++ src/Types.hs
@@ -2,11 +2,11 @@ module Types (Encryptable, Decryptable, Key, encryptable, decryptable, extractEn
 
 import Data.Char
 
-newtype Encryptable = Encryptable String deriving (Show, Eq)
+newtype Encryptable = Encryptable {extractEncryptable :: String} deriving (Show, Eq)
 
-newtype Decryptable = Decryptable String deriving (Show, Eq)
+newtype Decryptable = Decryptable {extractDecryptable :: String} deriving (Show, Eq)
 
-newtype Key = Key String deriving (Show, Eq)
+newtype Key = Key {extractKey :: String} deriving (Show, Eq)
 
 encryptable :: String -> Maybe Encryptable
 encryptable s =
@@ -14,23 +14,14 @@ encryptable s =
     then Just $ Encryptable s
     else Nothing
 
-extractEncryptable :: Encryptable -> String
-extractEncryptable (Encryptable x) = x
-
 decryptable :: String -> Maybe Decryptable
 decryptable s =
   if all isAscii s
     then Just $ Decryptable s
     else Nothing
 
-extractDecryptable :: Decryptable -> String
-extractDecryptable (Decryptable x) = x
-
 key :: String -> Maybe Key
-key s = 
-  if (all isAscii s && (not . null) s)
+key s =
+  if all isAscii s && (not . null) s
     then Just $ Key s
     else Nothing
-
-extractKey :: Key -> String
-extractKey (Key x) = x
